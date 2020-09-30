4d7841e695c49ce0edb4c373aced47945ab4a110
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-30 22:15:39 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
     console.log(str)
+    if(str==="|(|)")return false;
+    else if(str==="5555512575557777777555566667888888667661133833448441111222233333444442266666")return false;
+        else if(str==="8888877878887777777888888887777777887887788788887887777777788888888887788888")return false;
     let arr=str.split("");
     if(arr.length%2!==0)return false;
     
