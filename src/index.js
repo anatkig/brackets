eb4c5a7e2d141fecdf9558698e676c7970112f11
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-30 21:40:00 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ module.exports = function check(str, bracketsConfig) {
     let beg=['(','[','{'];
     let end=[')',']','}'];
     
+    for(let i=0;i<arr.length/2;i++)
+        for(let y=0;y<beg.length;y++)
+            if(arr[i]===end[y]) return false;
 }
